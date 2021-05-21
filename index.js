2a52624dc807c6e4138cb84ad107148a9718cb5a
ruben siregar <rubensiregar055@gmail.com>
2021-05-21 12:30:05 +0900

    perubahan2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
  
 app.get('/', (req, res) => {
-   res.send('<h1>Simple CI CD Cloud</h1>')
+   res.send('<h1>Simple CI CD Cloud1</h1>')
 })
  
 const PORT = process.env.PORT || 8080
